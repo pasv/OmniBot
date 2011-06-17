f2a75b1a623f85119fa3e1f5e55adf5162917c2a
Matt Howard <pasvninja@gmail.com>
2011-06-17 13:27:37 -0700

    adding OmniLib directory +__init__ and the OmniBot.py main source file

---
+++ OmniBot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+#
+# OmniBot main source file
+#
+import OmniLib
+import os
+import sys
+import re
+
+if __name__ == "__main__":
+    # And so begins the main
+    OmniLib.debug("[+] Initializing OmniBot!")
+    
