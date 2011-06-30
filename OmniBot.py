6f6d2bf2207c36ce9be7bee37a92b6060c2fd9c6
Matt Howard <pasvninja@gmail.com>
2011-06-30 05:21:38 -0700

    added GPL to head of Omni

--- OmniBot.py
+++ OmniBot.py
@@ -5,6 +5,20 @@
 # parse options to edit the Config object, start the threads for each
 # protocol and logging thread
 
+
+# OmniBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# OmniBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with OmniBot.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import sys
 import re,time
